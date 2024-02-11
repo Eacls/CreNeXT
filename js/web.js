09fcfb445599238b590945048096e899abaf1b92
Eacls <2652720816@qq.com>
2024-02-12 00:49:05 +0800

    update

--- js/web.js
+++ js/web.js
@@ -20,20 +20,3 @@ document.addEventListener('DOMContentLoaded', function() {
     mobileNav.classList.toggle('show');
   };
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const questions = document.querySelectorAll('.qa');
-
-    questions.forEach(question => {
-        question.addEventListener('click', function () {
-            this.classList.toggle('active');
-            const panel = this.nextElementSibling;
-
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + 'px';
-            }
-        });
-    });
-});
-
